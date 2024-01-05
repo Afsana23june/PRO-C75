8a5d27937779255327f6a1f302974b323565152b
Afsana23june <110661836+Afsana23june@users.noreply.github.com>
2024-01-05 09:49:52 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFn4QyDO35e-nPo0Ut5Ya_qhLHGtUEIzs",
+  authDomain: "pro-71-949d7.firebaseapp.com",
+  projectId: "pro-71-949d7",
+  storageBucket: "pro-71-949d7.appspot.com",
+  messagingSenderId: "244423794943",
+  appId: "1:244423794943:web:19b0e1061758fb21ba2cca",
+  measurementId: "G-7N2BFEHK84"
+};
